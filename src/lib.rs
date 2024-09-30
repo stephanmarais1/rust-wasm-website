0186b15bec8996aec98c3637027bbbcfc8f27ae7
Stephan Marais <hsm.stephan@gmail.com>
2024-09-30 20:55:56 +0200

    NOT WORKING YET

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,15 @@ use wasm_bindgen::prelude::*;
 
 // Expose the `greet` function to JavaScript
 #[wasm_bindgen]
-pub fn greet() {
-    web_sys::console::log_1(&"Hello, Ferris from Rust!".into());
+pub fn greeting(name: &str) -> JsValue {
+    JsValue::from_str("Hello, Ferris from Rust!")
 }
+
+#[wasm_bindgen]
+pub fn add(a: i32, b: i32) -> Result<i32, JsValue> {
+    Ok(a + b)
+}
+
 // #[cfg(test)]
 // mod tests {
 //     use super::*;
